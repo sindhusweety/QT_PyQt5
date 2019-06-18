655d5e32214a0249ae73d005525edca9d519f0e2
sindhu kumari <33718216+sindhusweety@users.noreply.github.com>
2019-06-18 16:53:51 -0400

    Add files via upload

---
+++ QT_PYQT5/main.cpp
@@ -0,0 +1,44 @@
+#include "Gaussianui.h"
+#include <QtWidgets/QApplication>
+#include <iostream>
+#include <QDebug>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+void LinearSpacedArray(float a, float b, float num)
+{
+	double min = -10;
+	double max = 10;
+	int n = 400;
+	vector<double> result;
+	// vector iterator
+	int iterator = 0;
+
+	for (int i = 0; i <= n - 2; i++)
+	{
+		double temp = min + i * (max - min) / (floor((double)n) - 1);
+		result.insert(result.begin() + iterator, temp);
+		iterator += 1;
+	}
+
+	//iterator += 1;
+
+	result.insert(result.begin() + iterator, max);
+	double arr_x0[400];
+	for (double i : result)
+	{
+		qDebug() << i << "\n";
+	}
+
+
+	system("pause");
+}
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	Gaussianui w;
+	LinearSpacedArray(10, 20, 30);
+	w.show();
+	return a.exec();
+}
